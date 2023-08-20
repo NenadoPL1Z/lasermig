5e31eb6d193ed098a8f65667e8fc0106f71d84ee
NenadoPL1Z <limon4ikpro1@gmail.com>
2023-08-20 23:47:18 +0500

    add: more pages

---
+++ src/pages/news/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Index = () => {
+  return (
+    <div>
+      <h1>news</h1>
+    </div>
+  );
+};
+
+export default React.memo(Index);
---
+++ src/pages/policy/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Index = () => {
+  return (
+    <div>
+      <h1>policy</h1>
+    </div>
+  );
+};
+
+export default React.memo(Index);
