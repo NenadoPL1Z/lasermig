b0d79bf16d9f3c0ffe42a4ea229ad866039df95d
NenadoPL1Z <limon4ikpro1@gmail.com>
2023-09-10 01:51:53 +0500

    update: home

--- src/components/Home/HomeCategories/index.styles.ts
+++ src/components/Home/HomeCategories/index.styles.ts
@@ -3,6 +3,7 @@ import { Breakpoints } from "@/styles/theme/Breakpoints";
 
 const ContainerSC = styled("ul")`
   display: grid;
+  grid-template-columns: 1fr 1fr;
   gap: 20px;
 
   list-style-type: none;
