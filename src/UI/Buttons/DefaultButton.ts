d91a24b70436fa597b8ba208e3c401d894c3860b
NenadoPL1Z <limon4ikpro1@gmail.com>
2023-09-10 12:20:35 +0500

    update: buttons

--- src/UI/Buttons/DefaultButton.ts
+++ src/UI/Buttons/DefaultButton.ts
@@ -2,11 +2,10 @@ import { Button, styled } from "@mui/material";
 import ColorScheme from "@/styles/theme/ColorScheme";
 import { FontNamespace } from "@/styles/theme/FontNamespace";
 import { Breakpoints } from "@/styles/theme/Breakpoints";
-import { iOS } from "@/lib/services";
 
 export const DefaultButton = styled(Button)`
   height: 60px;
-  padding: ${() => (iOS() ? 4 : 0)}px 30px 0;
+  padding: 0 30px;
 
   color: ${ColorScheme.PRIMARY};
   background-color: ${ColorScheme.ACCENT};
--- src/components/Home/HomeAbout/HASlider/index.tsx
+++ src/components/Home/HomeAbout/HASlider/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { HASliderProps } from "@/components/Home/types";
 import { styles } from "@/components/Home/HomeAbout/HASlider/index.styles";
+import { getImageUrl } from "@/lib/services";
 
 const HASlider = ({ slider, onSwiper }: HASliderProps) => {
   return (
     <SwiperSC loop={true} onSwiper={onSwiper} slidesPerView="auto">
       {slider.map((item) => (
         <SwiperSlideSC key={item}>
-          <ImgSC src="/assets/images/stanok.png" />
+          <ImgSC src={getImageUrl(item)} />
         </SwiperSlideSC>
       ))}
     </SwiperSC>
