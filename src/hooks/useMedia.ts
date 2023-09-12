8596fe7bdb5708a1df66601b6957554366c2d306
NenadoPL1Z <limon4ikpro1@gmail.com>
2023-09-12 22:51:12 +0500

    fix: media queries

--- src/hooks/useMedia.ts
+++ src/hooks/useMedia.ts
@@ -2,9 +2,9 @@ import { useMediaQuery } from "@mui/material";
 import { Breakpoints } from "@/styles/theme/Breakpoints";
 
 export const useNotebook = () =>
-  useMediaQuery(`@media (max-width: ${Breakpoints.NOTEBOOK}px`);
+  useMediaQuery(`(max-width:${Breakpoints.NOTEBOOK}px)`);
 
 export const useNotebookHD = () =>
-  useMediaQuery(`@media (max-width: ${Breakpoints.NOTEBOOK_HD}px`);
+  useMediaQuery(`(max-width:${Breakpoints.NOTEBOOK_HD}px)`);
 export const useMobile = () =>
-  useMediaQuery(`@media (max-width: ${Breakpoints.BIG_TABLET}px`);
+  useMediaQuery(`(max-width:${Breakpoints.BIG_TABLET}px)`);
