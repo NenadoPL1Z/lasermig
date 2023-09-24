f5d41697361b3cafbcadc4ecb15c732387e0f337
NenadoPL1Z <limon4ikpro1@gmail.com>
2023-09-24 16:43:38 +0500

    update

--- src/lib/mock/companyData.ts
+++ src/lib/mock/companyData.ts
@@ -3,6 +3,7 @@ export const companyData = {
   hrefPhone: "tel:89028935454",
   email: "sale@lasermig.ru",
   hrefEmail: "mailto:sale@lasermig.ru",
-  address: "Г. Челябинск,\nул. Худякова 421, офис 503",
+  address:
+    "454018, Челябинская область,\nг Челябинск, ул Братьев Кашириных,\nд. 54, помещ. 3/17а",
   workTime: "Пн-Пт: 9:30-18:30\nCб-Вс: Выходной",
 };
